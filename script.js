2365b5dd48f5952c8e45e6e0c262ca03c08336f4
manishgujjar5299 <manishgujjar5299@gmail.com>
2024-06-08 15:23:32 +0530

    hero

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.querySelector('.menu-icon');
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const toggleBtn = document.querySelector('.menu-icon');
     const navLinks = document.querySelector('.nav-links');
-  
-    menuIcon.addEventListener('click', function() {
-      navLinks.classList.toggle('active');
+
+    toggleBtn.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
     });
-  });
-  
+
+    // Hide nav-links when a link is clicked (for better UX)
+    const links = document.querySelectorAll('.nav-links a');
+    links.forEach(link => {
+        link.addEventListener('click', function() {
+            navLinks.classList.remove('active');
+        });
+    });
+});
